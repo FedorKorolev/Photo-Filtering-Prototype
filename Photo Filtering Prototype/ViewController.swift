eb69d5637782332cce2289451604ee30dac8ec03
FedorKorolev <korolev.f@icloud.com>
2017-03-28 19:37:03 +0300

    Initial Commit

---
+++ Photo Filtering Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Filtering Prototype
+//
+//  Created by Фёдор Королёв on 28.03.17.
+//  Copyright © 2017 Фёдор Королёв. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
